25369108b7bfd2c71de5fc6341be438b97528cd0
Domagoj Jurenec <domagoj@aboutdream.io>
2023-10-28 18:18:36 +0200

    end of section 10

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { CoreModule } from './core/core.module';
+
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    CoreModule,
+
+    HomeModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
